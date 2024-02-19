3aa073b5267ce6496503987b7c751c53eec6537a
VICMK7 <145030290+VICMK7@users.noreply.github.com>
2024-02-19 12:50:05 -0500

    Update gate.h

--- Map/map/gate.h
+++ Map/map/gate.h
@@ -5,10 +5,34 @@
 
 class Gate : public Tile
 {
+private:
+	int width;
+	int height;
+	State state;
+	Orientation orientation;
+	Controller controller;
+
 public:
 	Gate(int x, int y);
 	~Gate();
+
 	void Show();
+
+	// Getters
+	int getWidth();
+	int getHeight();
+	int getDimension();
+	State getState();
+	Orientation getOrientation();
+	Controller getController();
+	
+	//Setters
+	void setWidth(width);
+	void setHeight(height);
+	void setDimension(width, height);
+	void setState(state);
+	void setOrientation(orientation);
+	void setController(controller);
 };
 
 #endif GATE_H
