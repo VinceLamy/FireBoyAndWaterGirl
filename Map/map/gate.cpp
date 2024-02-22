8ad6a9123a0864f59463957ab2c5a8c97e1cffd7
VICMK7 <vic.rod77@outlook.com>
2024-02-22 11:31:16 -0500

    update gate

--- Map/map/gate.cpp
+++ Map/map/gate.cpp
@@ -1,19 +1,85 @@
 #include "gate.h"
 #include <iostream>
 
-using namespace	std;
+using namespace std;
 
 Gate::Gate(int x, int y)
 {
-	SetPostition(x, y);
-	SetType(GATE);
+    SetPosition(x, y);
+    SetType(GATE);
 }
 
 Gate::~Gate()
 {
 }
 
+int Gate::getWidth() 
+{
+    return width;
+}
+
+int Gate::getHeight()
+{
+    return height;
+}
+
+pair<int, int> Gate::getDimension()
+{
+    return make_pair(width, height);
+}
+
+State Gate::getState()
+{
+    return state;
+}
+
+Orientation Gate::getOrientation()
+{
+    return orientation;
+}
+
+int Gate::getSpeed()
+{
+    // Implementation
+}
+
+Controller Gate::getController()
+{
+    // Implementation
+}
+
+void Gate::setWidth(width)
+{
+    this->width = width;
+}
+
+void Gate::setHeight(height)
+{
+    this->height = height;
+}
+
+void Gate::setDimension(width, height)
+{
+    this->width = width;
+    this->height = height;
+}
+
+void Gate::setState(state)
+{
+    this->state = state;
+}
+
+void Gate::setOrientation(orientation)
+{
+    this->orientation = orientation;
+}
+
+void Gate::setController(controller)
+{
+    this->controller = controller;
+}
+
 void Gate::Show()
 {
-	cout << '|';
+    cout << '|';
 }
